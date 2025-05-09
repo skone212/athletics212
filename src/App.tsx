83c0f66ad8fd88f51aebdc526463b9dd2385f0d4
Sebastian Kneifel <sebastian.kneifel@me.com>
2025-05-09 11:57:49 +0200

    Add /login route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import RoutineDetail from "./pages/RoutineDetail";
-import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/routine/:id" element={<RoutineDetail />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* weitere Routen */}
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
